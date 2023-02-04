45c2b597d5dee5d0402bd964b39f612137f313f6
minhquoc1423@gmail.com <minhquoc1423@gmail.com>
2023-02-04 20:30:57 +0700

    set up auth controller for signin&&out and auth

--- server/express.js
+++ server/express.js
@@ -7,6 +7,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 // import Template from '../template'
 import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
@@ -21,6 +22,15 @@ app.use(cors());
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 app.use('/', userRoutes);
+app.use('/', authRoutes)
 
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({"error": err.name + ": " + err.message})
+    }else if (err) {
+        res.status(400).json({"error": err.name + ": " + err.message})
+        console.log(err)
+    }
+})
 
 export default app
