02cf9e3ccc2856254f0da2d3d9ceb9dcd2803873
Vo Minh Quoc (Sagit) <84282841+Sagit612@users.noreply.github.com>
2023-02-12 23:01:07 +0700

    finished setting up Profile function for client

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -5,12 +5,14 @@ import Users from "./user/Users";
 import Signin from "./auth/Signin";
 import Menu from "./core/Menu";
 import Signup from "./user/Signup";
+import Profile from "./user/Profile";
 
 const path = {
     home: '/',
     users: '/users',
     signin: '/signin',
     signup: '/signup',
+    profile: '/user/:userId'
 }
 
 export default function MainRouter () {
@@ -21,6 +23,7 @@ export default function MainRouter () {
             <Route path={path.users} component={Users}/>
             <Route path={path.signin} component={Signin}/>
             <Route path={path.signup} component={Signup}/>
+            <Route path={path.profile} component={Profile}/>
         </Switch>
     </div>)
 }
--- client/user/api-user.js
+++ client/user/api-user.js
@@ -26,7 +26,25 @@ const create = async (user) => {
     }
 }
 
+const read = async (params, credentials, signal) => {
+    try {
+        let response = await fetch('/api/users/' + params.userId, {
+            method: 'GET',
+            signal: signal,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            }
+        })
+        return await response.json()
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 export {
     create,
-    list
+    list,
+    read
 }
